a971a647fc5b55081d1657b360d53d02bcf309d4
Elia <emb083@latech.edu>
2026-04-11 12:21:27 -0500

    customer spawning and random sprites

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -10,13 +10,24 @@ public class Customer : MonoBehaviour {
 
     private Sprite realitySprite;
     private Sprite imagineSprite;
+    private GameBehavior Game;
 
     void Start(){
+        Game = GameBehavior.Instance;
+        SpriteRenderer renderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+
         // setting randomized sprites
         int realityRand = Random.Range(0, realitySpriteOps.Count);
         realitySprite = realitySpriteOps[realityRand];
 
         int imagineRand = Random.Range(0, imagineSpriteOps.Count);
         imagineSprite = imagineSpriteOps[imagineRand];
+
+        if (Game.currentState == GameBehavior.MindState.REALITY || Game.currentState == GameBehavior.MindState.REALITY_LOCKED){
+            renderer.sprite = realitySprite;
+        }
+        else if (Game.currentState == GameBehavior.MindState.IMAGINATION || Game.currentState == GameBehavior.MindState.IMAGINATION_LOCKED){
+            renderer.sprite = imagineSprite;
+        }
     }
 }
